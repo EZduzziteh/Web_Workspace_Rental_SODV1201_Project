24a5bf7a7b489430824016476a8a1f8079aef95e
Paul K <64181231+BVC414800@users.noreply.github.com>
2022-06-03 08:56:15 -0600

    Blank Website Templates
    These files are the base for our website.  We are open to adding codes to them when available. Ideally we can use these templates to design and code our project together.

---
+++ sodv1201termproject.js
@@ -0,0 +1,28 @@
+/* SODV 1201 Introduction to Web Programming 
+SODV 1201 Term Project- Co-Worker Work Spaces and Properties 
+ Program designed and coded by: SODV 1201 Group 2: Sasha Greene,  Gurleen Kaur, Paul K Kho, Evan Maclean
+ Instructor:                   Dima Marachi
+ Due       :     Phase 1   : June 2 2022
+                 Phase 2   : June 24 2022 
+*/
+
+
+// Landing Page (Home Page )
+
+
+
+// User Story 1 and 3 
+
+
+
+
+// User Story 2 and 4
+
+
+
+// User Story 5 and 6
+
+
+
+// User Story 7 and 8
+
