c4c81bc82f9ea59fe89bf61e9e3b0a5e14f827c5
Paul K <64181231+BVC414800@users.noreply.github.com>
2022-06-15 11:52:18 -0600

    Group 2 Project Server Files
    These files serve as beginning of  our server.

---
+++ projectserver.js
@@ -0,0 +1,14 @@
+/* SODV 1201 Introduction to Web Programming 
+SODV 1201 Term Project- Co-Worker Work Spaces and Properties 
+ Program designed and coded by: SODV 1201 Group 2: Sasha Greene,  Gurleen Kaur, Paul K Kho, Evan Maclean
+ Instructor:                   Dima Marachi
+ Due       :     Phase 1   : June 2 2022
+                 Phase 2   : June 24 2022 
+*/
+
+const express = require("express");
+
+const app = express();
+
+const port = 8080;
+
